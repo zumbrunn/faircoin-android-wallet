8d6477ab9c3b8cb7ced132725c179b6abd0cde7b
Thomas König <tom@fair-coin.org>
2017-04-04 09:19:27 +0200

    adapt 'address' URL for FairCoin2 block explorer

--- wallet/src/de/schildbach/wallet/ui/WalletAddressesFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletAddressesFragment.java
@@ -180,7 +180,7 @@ public final class WalletAddressesFragment extends FancyListFragment {
 
                 case R.id.wallet_addresses_context_browse:
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.withAppendedPath(config.getBlockExplorer(),
-                            "address/" + getAddress(position).toBase58())));
+                            "address?address=" + getAddress(position).toBase58())));
 
                     mode.finish();
                     return true;
